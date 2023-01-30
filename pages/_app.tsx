e09e087cf203b9acc06504fbd43c97c71c92b9b4
raslan <24810123+raslan@users.noreply.github.com>
2023-01-30 21:59:15 +0200

    feat: add basic pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Playfair_Display, Inter } from '@next/font/google';
+
+const playfair = Playfair_Display({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-playfair',
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div
+      className={`${inter.variable} ${playfair.variable} bg-black p-0 m-0 box-border text-white flex min-h-screen overflow-hidden`}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
